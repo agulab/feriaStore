613f39e86ff995c4a16882d141e2ea547ef7d04b
Agustín Labaronnie <agulab@gmail.com>
2022-02-19 00:14:02 +0100

    Agrego /items con sort, page y limit

--- feriaStore.py
+++ feriaStore.py
@@ -3,16 +3,25 @@ import falcon.asgi as asgi
 import mongo_connector
 import uvicorn
 import pymongo
+import json
 
 class ItemsResource:
     async def on_get(self, req: asgi.Request, resp: asgi.Response):
         """Handles GET requests"""
-        page = req.get_param_as_int("page") if req.get_param_as_int("page") else 1
-        limit = req.get_param_as_int("limit") if req.get_param_as_int("limit") else 5
+        toPage = (req.get_param_as_int("page")-1) * limit if req.get_param_as_int("page") else 0
+        limit = req.get_param_as_int("limit") if req.get_param_as_int("limit") else 10
+        sort = req.get_param("sort") if req.get_param("sort") else None
+        if sort:
+            cursor = dbClient.get_default_database().get_collection("items").find({}, {'_id': False}).sort(sort).skip(toPage).limit(limit)
+        else:
+            cursor = dbClient.get_default_database().get_collection("items").find({}, {'_id': False}).skip(toPage).limit(limit)
 
-        cursor = dbClient.get_default_database().get_collection("items").find()
         resp.status = falcon.HTTP_200
-        resp.body = cursor[(page-1) * limit : page * limit]
+        items = []
+        for item in cursor:
+            items.append(item)
+
+        resp.text = json.dumps(items)
 
 class ItemResource:
     async def on_get(self, req: asgi.Request, resp: asgi.Response):
@@ -20,10 +29,10 @@ class ItemResource:
         query = {"id": id}
         print(query)
         try:
-            item = dbClient.get_default_database().get_collection("items").find_one(query)
+            item = dbClient.get_default_database().get_collection("items").find_one(query, {'_id': False})
             print(item)
             resp.status = falcon.HTTP_200
-            resp.text = item
+            resp.text = json.dumps(item)
         except Exception as e:
             print(str(type(e)) + str(e))    
         
