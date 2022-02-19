89d9db3cf53bfdb16487cd229180cafcb415f14d
Agustín Labaronnie <agulab@gmail.com>
2022-02-19 19:09:59 +0100

    Agrego endpoint /toStock para marcar un item como terminado y pasarlo de toDo a stock

--- feriaStore.py
+++ feriaStore.py
@@ -25,26 +25,56 @@ class ItemsResource:
 
 class ItemResource:
     async def on_get(self, req: asgi.Request, resp: asgi.Response, id):
-        query = {"id": id}
-        print(query)
         try:
-            item = dbClient.get_default_database().get_collection("items").find_one(query, {'_id': False})
-            print(item)
+            item = self.getItem(id)
             resp.status = falcon.HTTP_200
             resp.text = json.dumps(item)
         except Exception as e:
             print(str(type(e)) + str(e))    
-        
+
+    def getItem(self, id):
+        query = {"id": id}
+        return dbClient.get_default_database().get_collection("items").find_one(query, {'_id': False})
+
+
 dbClient = mongo_connector.dbClient
+
+class ItemToStockResource:
+    async def on_put(self, req: asgi.Request, resp: asgi.Response, id):
+        if (not itemResource.getItem(id)):
+            resp.status = falcon.HTTP_404
+        else:
+            n = 1 if not req.get_param_as_int("n") else req.get_param_as_int("n")
+            query = {"$inc":{"stock":n, "todo":-n}}
+            updatedItem = dbClient.get_default_database().get_collection("items").find_one_and_update(
+                {"id":id, "todo": {"$gte":n}}, query, {'_id':False}, return_document=pymongo.ReturnDocument.AFTER)
+            if(updatedItem):
+                resp.status = falcon.HTTP_200
+                resp.text = json.dumps(updatedItem)
+            else:
+                resp.status = falcon.HTTP_409
+
+
+
 
 # Api
 api = asgi.App()
-items = ItemsResource()
-item = ItemResource()
+itemsResource = ItemsResource()
+itemResource = ItemResource()
+itemToStockResource = ItemToStockResource()
 
-# Endpoints
-api.add_route('/items', items)
-api.add_route('/items/{id:int}', item)
+
+### Endpoints ###
+# GET: Devuelve lista de items
+api.add_route('/items', itemsResource) 
+
+# GET: Devuelve el item {id}
+api.add_route('/items/{id:int}', itemResource) 
+
+# PUT: Pasa al stock un item {id} que estaba por hacerse.
+# Puede recibir el parámetro 'n' para especificar la cantidad
+# de items que deben pasar al stock.
+api.add_route('/items/{id:int}/toStock', itemToStockResource) 
 
 
 # For debugging purposes only
