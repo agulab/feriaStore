e94a03a0303d6bc77e4bc6c56f6e533f492d2d4e
Agustín Labaronnie <agulab@gmail.com>
2022-02-21 21:58:37 +0100

    Conexión MongoDB funcionando con 'secrets'

--- mongo_connector.py
+++ mongo_connector.py
@@ -1,12 +1,15 @@
 import logging
+from dotenv import dotenv_values
 
 from pymongo import errors
 from pymongo.mongo_client import MongoClient
 from pymongo.read_preferences import ReadPreference
 
+secrets = dotenv_values("secrets")
 # Default options
 __config = {
-    'uri': 'mongodb://localhost:27017/feria',
+    #'uri': 'mongodb://localhost:27017/feria',
+    'uri': 'mongodb+srv://feriaAdmin:'+secrets["dbPass"]+'@cluster0.axfed.mongodb.net/feria?retryWrites=true&w=majority',
     'fsync': False,
     'write_concern': 0,
     'separate_collections': False,
