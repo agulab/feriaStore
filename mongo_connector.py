bdedf45a7aac52ee8abbbace030ec35f2e882599
Agustín Labaronnie <agulab@gmail.com>
2022-02-23 02:19:57 +0100

    en main queda el mongo_connector correcto

--- mongo_connector.py
+++ mongo_connector.py
@@ -6,8 +6,8 @@ from pymongo.read_preferences import ReadPreference
 
 # Default options
 __config = {
-    'uri': 'mongodb://localhost:27017/feria',
-    #'uri': 'mongodb+srv://feriaAdmin:'+str(os.environ.get("dbPass"))+'@cluster0.axfed.mongodb.net/feria?retryWrites=true&w=majority',
+    #'uri': 'mongodb://localhost:27017/feria',
+    'uri': 'mongodb+srv://feriaAdmin:'+str(os.environ.get("dbPass"))+'@cluster0.axfed.mongodb.net/feria?retryWrites=true&w=majority',
     'fsync': False,
     'write_concern': 0,
     'separate_collections': False,
