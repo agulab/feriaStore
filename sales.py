e609a0f6e679d8a17abd5a2224123e57fd31a633
Agustín Labaronnie <agulab@gmail.com>
2022-06-10 14:21:17 +0200

    Actualizo sales para incluir 'inv' y ya no usar 'size'

--- sales.py
+++ sales.py
@@ -13,7 +13,7 @@ class SalesResource:
         toPage = (req.get_param_as_int("page")-1) * limit if req.get_param_as_int("page") else 0
         sort = req.get_param("sort") if req.get_param("sort") else "date"
         asc = pymongo.ASCENDING if req.get_param_as_bool("asc") else pymongo.DESCENDING
-        if sort in ["name","size"]:
+        if sort in ["name"]:
             sort = "item." + sort
 
         cursor = dbClient.get_default_database().get_collection("sales").aggregate([
@@ -24,7 +24,6 @@ class SalesResource:
         sales = []
         for sale in cursor:
             sale["name"] = sale["item"][0]["name"]
-            sale["size"] = sale["item"][0]["size"]
             del sale["item"]
             del sale["_id"]
             sales.append(sale)
@@ -44,7 +43,8 @@ class SalesResource:
                 resp.text = "Imposible crear venta de item sin stock"
             else:
                 try:
-                    sale = Sale(saleReq)
+                    invId = req.context.auth["user"]["id"]
+                    sale = Sale(saleReq, invId)
                     dbClient.get_default_database().get_collection("sales").insert_one(sale)
                     dbClient.get_default_database().get_collection("items").find_one_and_update({"id":item["id"]},{"$inc":{"stock":-1}})
                     
@@ -83,10 +83,11 @@ class SaleResource:
 
             
 class Sale(dict):
-    def __init__(self, sale):
+    def __init__(self, sale, invId):
         checkDate = re.compile("\d{4}-\d{2}-\d{2}")
         
         # Atributos requeridos
+        self["inv"] = int(invId)
         self["price"] = int(sale["price"])
         self["itemId"] = int(sale["itemId"])
         if checkDate.match(sale["date"]):
